00ced6d11f4a0378e1f4364bf252b1069a0da481
Yuwei Huang <yuwaiw@gmail.com>
2020-12-11 21:20:48 -0800

    Initial Commit

---
+++ src/streamer/main.cpp
@@ -0,0 +1,7 @@
+#include <drc-streaming/internal/video-converter.h>
+#include <drc-streaming/internal/h264-encoder.h>
+
+int main(int argc, char const *argv[]) {
+  drc::VideoConverter vc;
+  drc::H264Encoder enc;
+}
